6211b5ea443f502447d69bdf21c0a3c5b24e7690
Cora Hörstmann <choerstm@dmawi.de>
2021-07-16 11:15:56 +0200

    replace NA with bottom depth for well mixed samples

--- MLD_calculations.R
+++ MLD_calculations.R
@@ -1,17 +1,15 @@
 #MLD calculations
-source("C:/Users/choerstm/Documents/Studenten/arctic_picos/Submission/MLD_DK.R")
-
-cTD_HE533 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/HE533_CTD.txt", sep = "\t")
-CTD_HE492 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/HE492_CTD.txt", sep = "\t")
-CTD_HE431 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/HE431_CTD.txt", sep = "\t")
-CTD_MSM56 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/MSM56_CTD.txt", sep = "\t")
-CTD_MSM21 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/MSM21_CTD.txt", sep = "\t")
+cTD_HE533 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/HE533_CTD.txt", sep = "\t",  encoding = "UTF-8", stringsAsFactors=FALSE)
+CTD_HE492 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/HE492_CTD.txt", sep = "\t",  encoding = "UTF-8", stringsAsFactors=FALSE)
+CTD_HE431 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/HE431_CTD.txt", sep = "\t",  encoding = "UTF-8", stringsAsFactors=FALSE)
+CTD_MSM56 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/MSM56_CTD.txt", sep = "\t",  encoding = "UTF-8", stringsAsFactors=FALSE)
+CTD_MSM21 <- read.csv("C:/Users/choerstm/Documents/Studenten/arctic_picos/CTD/MSM21_CTD.txt", sep = "\t",  encoding = "UTF-8", stringsAsFactors=FALSE)
 #HE533_1 <- cTD_HE533%>%group_split(Event)
 
 HE533 <- dplyr::inner_join(meta_map_1, cTD_HE533, by = c("Latitude", "Longitude"))
 HE533_MLD <- HE533%>%group_by(Event)%>%
   summarise(MLD = MLD_DK(depth = Depth.water..m., variable = Temp....C., threshold = 0.5, 
-                   print.info = TRUE, plot = TRUE, depth.max = 1000), bottom_depth = max(Elevation..m.), 
+                   print.info = F, plot = F, depth.max = 1000), bottom_depth = max(Elevation..m.), 
             Latitude = mean(Latitude), Longitude = mean(Longitude))
 
 
@@ -31,16 +29,17 @@ HE431_MLD <- HE431%>%group_by(Event)%>%
 
 HE431_MLD_s <- HE431_MLD[!(HE431_MLD$Event == "HE431/28-1"|HE431_MLD$Event == "HE431/29-1" | HE431_MLD$Event == "HE431/31-1"),] 
 
-
-MSM21 <- dplyr::inner_join(meta_map_1, CTD_MSM21, by = c("Latitude", "Longitude"))
-MSM21$Temp....C. <- MSM21$Temp..Â.C...ITS.90..Sensor.1..CTD..SEA.BI....
+CTD_MSM21$Station <- gsub("/3_", ".F02.", CTD_MSM21$Event)
+CTD_MSM21$Station <- gsub("-.*", "", CTD_MSM21$Station)
+MSM21 <- dplyr::inner_join(meta_map_1, CTD_MSM21, by = "Station")
+MSM21$Temp....C. <- MSM21$Temp...C...ITS.90..Sensor.1..CTD..SEA.BI....
 MSM21_MLD <- MSM21%>%group_by(Event)%>%
   summarise(MLD = MLD_DK(depth = Depth.water..m., variable = Temp....C., threshold = 0.5, 
                          print.info = F, plot = F, depth.max = 1000), bottom_depth = max(Elevation..m.),
-            Latitude = mean(Latitude), Longitude = mean(Longitude))
+            Latitude = mean(Latitude.x), Longitude = mean(Longitude.x))
 
 MSM56 <- dplyr::inner_join(meta_map_1, CTD_MSM56, by = c("Latitude", "Longitude"))
-MSM56$Temp....C. <- MSM56$Temp..Â.C...Sensor.1..ITS.90..CTD..SEA.BI....
+MSM56$Temp....C. <- MSM56$Temp...C...Sensor.1..ITS.90..CTD..SEA.BI....
 MSM56_MLD <- MSM56%>%group_by(Event)%>%
   summarise(MLD = MLD_DK(depth = Depth.water..m., variable = Temp....C., threshold = 0.5, 
                          print.info = F, plot = F, depth.max = 1000), bottom_depth = max(Elevation..m.),
@@ -51,6 +50,15 @@ all_MLD <- rbind(HE533_MLD, HE492_MLD, MSM21_MLD, MSM56_MLD, HE431_MLD_s)
 meta_map_MLD <- dplyr::left_join(meta_map_1, all_MLD, by = c("Latitude", "Longitude"))
 #now the calculate MLD needs to be added to metadata tables
 
-meta_16S_m <- left_join(meta_16S, meta_map_MLD[, c("Station", "MLD", "bottom_depth")], by = "Station")
+meta_16S_m <- left_join(meta_16S_nNA, meta_map_MLD[, c("Station", "MLD", "bottom_depth")], by = "Station")
+meta_18S_m <- left_join(meta_18S_nNA, meta_map_MLD[, c("Station", "MLD", "bottom_depth")], by = "Station")
+
+
+#those samples with well mixed need to be turned into numbers
+
+meta_16S_m$MLD <- ifelse(is.na(meta_16S_m$MLD), -(meta_16S_m$bottom_depth), meta_16S_m$MLD)
+meta_18S_m$MLD <- ifelse(is.na(meta_18S_m$MLD), -(meta_18S_m$bottom_depth), meta_18S_m$MLD)
+
 
 rm(all_MLD, MSM21, MSM21_MLD, MSM56, MSM56_MLD, HE431, HE431_MLD, HE431_MLD_s, HE533, HE533_MLD, HE492, HE492_MLD)
+rm(CTD_HE431, CTD_HE492, CTD_MSM21, CTD_MSM56, cTD_HE533)
