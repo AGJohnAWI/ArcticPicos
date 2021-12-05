956c2a613a4e074c127549ddcef370a5e4dc1d28
Cora Hörstmann <choerstm@dmawi.de>
2021-12-05 12:35:29 +0100

    directory update 1

--- README.md
+++ README.md
@@ -1,15 +1,23 @@
 # ArcticPicos
 
+**microbial community analyses of arctic fjords**
+
 community analyses of arctic fjords
 
-all import files can be found under:
+### all import files can be found under: 
+
 data_import
 
-data transformations are compiled in the folder
+### data transformations are compiled in the folder
+
 data_transformations
 
-data analysis is divided into multiple folders
-analysis_multivariate #this folder contains the RDA analysis
-analysis_distance # this folder contains the correlation analysis between oceanographic connectivity and microbial beta diversity
-analysis_network # this folder contains the network analysis of the pro-and eukaryotic networks between glacier and non-glacier influenced regions
-env_calc # this is the code for the plots of the environmental data 
+### data analysis is divided into multiple folders 
+
+analysis_multivariate #this folder contains the RDA analysis 
+
+analysis_distance # this folder contains the correlation analysis between oceanographic connectivity and microbial beta diversity 
+
+analysis_network # this folder contains the network analysis of the pro-and eukaryotic networks between glacier and non-glacier influenced regions 
+
+env_calc # this is the code for the plots of the environmental data
---
+++ env_calc/meta_reduction.R
@@ -0,0 +1,45 @@
+##prokaryotes
+
+#replace below detection with actual number
+meta_16S$PO4_umol.l <- gsub("<LOQ","0.001", meta_16S$PO4_umol.l)
+meta_16S$Si_umol.l <- gsub("<LOQ","0.001", meta_16S$Si_umol.l)
+
+meta_16S$PO4_umol.l <- as.numeric(meta_16S$PO4_umol.l)
+meta_16S$Si_umol.l <- as.numeric(meta_16S$Si_umol.l)
+
+
+#remove NAs and reduce to relevant metadata
+
+metadata_16S <- c("Site", "Station", "Region", "Fjord","In.Out", "Sill", "Latitude", "Longitude", "Date", "Time",
+                  "temperature...C.", "salinity..psu.","O2umol.l", "Fluorometer",
+                  "PO4_umol.l", "Si_umol.l", "NO3_umol.l",
+                  "Glacial.influence", "Current_flow", "Current_score")
+
+meta_16S_nNA <-meta_16S[metadata_16S]%>% 
+  mutate_all(~ifelse(. %in% c("N/A", "null", ""), NA, .)) %>% 
+  na.omit()
+
+
+##eukaryotes
+
+#replace below detection with actual number
+meta_18S_r$PO4_umol.l <- gsub("<LOQ","0.001", meta_18S_r$PO4_umol.l)
+meta_18S_r$Si_umol.l <- gsub("<LOQ","0.001", meta_18S_r$Si_umol.l)
+
+meta_18S_r$PO4_umol.l <- as.numeric(meta_18S_r$PO4_umol.l)
+meta_18S_r$Si_umol.l <- as.numeric(meta_18S_r$Si_umol.l)
+
+
+#remove NAs and reduce to relevant metadata
+
+metadata_18S <- c("Site","Station", "Region", "Fjord","In.Out", "Sill", "Latitude", "Longitude", "Date", "Time",
+                  "Temperature...C.", "Salinity..psu.","O2umol.l", "Fluorometer",
+                  "PO4_umol.l", "Si_umol.l", "NO3_umol.l",
+                  "Glacial.influence", "Current_flow", "Current_score")
+
+meta_18S_nNA <-meta_18S_r[metadata_18S]%>% 
+  mutate_all(~ifelse(. %in% c("N/A", "null", ""), NA, .)) %>% 
+  na.omit()
+
+meta_map_1 <- dplyr::full_join(meta_16S_nNA[,c("Station", "Glacial.influence", "Latitude", "Longitude")], meta_18S_nNA[,c("Station", "Glacial.influence", "Latitude", "Longitude")])
+meta_map <- meta_map_1
