4e2d0364526ee5697eba416a3f020299b99c6ea3
Cora Hoerstmann <corahoerstmann@Coras-MacBook-Pro.local>
2023-04-04 18:12:25 +0200

    remove analysis or glacial influence

--- alpha_div/boxplot_alphadiv.R
+++ alpha_div/boxplot_alphadiv.R
@@ -3,9 +3,11 @@ packageVersion("ggplot2")
 
 s <- c("Richness.euk", "Richness.prok")
 div_both_s <- list_div$div_alpha_both%>%dplyr::filter(Div_indices %in% s)
-
+div_both_s$BioZone <- div_both_s$Bioclimatic_subzone
+div_both_s$BioZone <- gsub("high_", "",div_both_s$BioZone)
+div_both_s$BioZone <- gsub("low_", "",div_both_s$BioZone)
 box_plot_A <- ggplot(data = div_both_s, 
-                    aes(x= Glacial.influence, y=Div_value, fill=Div_indices)) +
+                    aes(x= Bioclimatic_subzone, y=Div_value, fill=Div_indices)) +
   stat_boxplot(geom ='errorbar', width = 0.6) +
   geom_boxplot(width = 0.6) +
   xlab("Glacial influence") +
@@ -22,44 +24,30 @@ box_plot_A <- ggplot(data = div_both_s,
 print(box_plot_A)
 
 #test whether two samples randomly taken belong to the same group
-div_euk_s_G <- list_div$euk_r_alpha_n%>%dplyr::filter(Glacial.influence == "Yes")
-div_euk_s_NG <- list_div$euk_r_alpha_n%>%dplyr::filter(Glacial.influence == "No")
+div_euk_s_G <- list_div$euk_r_alpha_n%>%dplyr::filter(Bioclimatic_subzone == "high_arctic")
+div_euk_s_NG <- list_div$euk_r_alpha_n%>%dplyr::filter(Bioclimatic_subzone == "low_arctic")
 wilcox.test(div_euk_s_G$Richness.euk, div_euk_s_NG$Richness.euk, alternative = "two.sided")
 
-div_prok_s_G <- list_div$prok_alpha_n%>%dplyr::filter(Glacial.influence == "Yes")
-div_prok_s_NG <- list_div$prok_alpha_n%>%dplyr::filter(Glacial.influence == "No")
+anova_prok <- aov(Richness.prok ~ Bioclimatic_subzone, data = list_div$prok_alpha_n)
+anova_prok
 
-wilcox.test(div_prok_s_G$Richness.prok, div_prok_s_NG$Richness.prok, alternative = "two.sided")
+anova_euk <- aov(Richness.euk ~ Bioclimatic_subzone, data = list_div$euk_r_alpha_n)
+anova_euk
 
-res.ftest <- var.test(Richness.prok ~ Glacial.influence, data = list_div$prok_alpha_n)
-res.ftest
+print(pairwise.t.test(list_div$euk_r_alpha_n$Richness.euk, list_div$euk_r_alpha_n$Bioclimatic_subzone, var.equal = FALSE, p.adjust.method='bonferroni'))
+print(pairwise.t.test(list_div$euk_r_alpha_n$Pielou.euk, list_div$euk_r_alpha_n$Bioclimatic_subzone, var.equal = FALSE, p.adjust.method='bonferroni'))
 
-print(t.test(div_prok_s_G$Richness.prok, div_prok_s_NG$Richness.prok, var.equal = FALSE))
-
-## mean and standard deviation from the NG Richness
-
-mean(div_prok_s_NG$Richness.prok)
-sd(div_prok_s_NG$Richness.prok)
-
-mean(div_euk_s_NG$Richness.euk)
-sd(div_euk_s_NG$Richness.euk)
-##to addd the level of "In.Out"
-
-#p <- ggplot(data = div_both_s, aes(x = In.Out, y = Div_value, fill = Div_indices))
-#p <- p + geom_boxplot(width = 0.5, position = "dodge")
-#p <- p + facet_grid(. ~ Glacial.influence)
-#p <- p + theme_bw()
-#p <- p + theme(axis.text.x = element_text(angle = 90))
-#p
-
-#clean u!
+print(pairwise.t.test(list_div$prok_alpha_n$Richness.prok, list_div$prok_alpha_n$Bioclimatic_subzone, var.equal = FALSE, p.adjust.method='bonferroni'))
+print(pairwise.t.test(list_div$prok_alpha_n$Pielou.prok, list_div$prok_alpha_n$Bioclimatic_subzone, var.equal = FALSE, p.adjust.method='bonferroni'))
 
 
 s <- c("Pielou.euk", "Pielou.prok")
 div_both_s <- list_div$div_alpha_both%>%dplyr::filter(Div_indices %in% s)
-
+div_both_s$BioZone <- div_both_s$Bioclimatic_subzone
+div_both_s$BioZone <- gsub("high_", "",div_both_s$BioZone)
+div_both_s$BioZone <- gsub("low_", "",div_both_s$BioZone)
 box_plot_A <- ggplot(data = div_both_s, 
-                     aes(x= Glacial.influence, y=Div_value, fill=Div_indices)) +
+                     aes(x= Bioclimatic_subzone, y=Div_value, fill=Div_indices)) +
   stat_boxplot(geom ='errorbar', width = 0.6) +
   geom_boxplot(width = 0.6) +
   xlab("Glacial influence") +
@@ -74,20 +62,6 @@ box_plot_A <- ggplot(data = div_both_s,
         panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
 print(box_plot_A)
-
-wilcox.test(div_euk_s_G$Pielou.euk, div_euk_s_NG$Pielou.euk, alternative = "two.sided")
-wilcox.test(div_prok_s_G$Pielou.prok, div_prok_s_NG$Pielou.prok, alternative = "two.sided")
-
-print(var.test(Pielou.prok ~ Glacial.influence, data = list_div$prok_alpha_n))
-
-print(t.test(div_prok_s_G$Pielou.prok, div_prok_s_NG$Pielou.prok, var.equal = TRUE))
-
-#eukaryotes
-
-print(var.test(Pielou.euk ~ Glacial.influence, data = list_div$euk_r_alpha_n))
-
-print(t.test(div_euk_s_G$Pielou.euk, div_euk_s_NG$Pielou.euk, var.equal = FALSE))
-
 
 rm(box_plot_A, div_both_s)
 
